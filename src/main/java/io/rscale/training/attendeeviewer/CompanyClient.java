cf8a75297c9c30ac2e559be9a27fae9835bb6e69
Brian Berg <brian.berg@cgi.com>
2017-09-14 09:42:11 -0600

    init push

--- src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
+++ src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
@@ -1,5 +1,8 @@
 package io.rscale.training.attendeeviewer;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+@FeignClient(name = "company")
 public interface CompanyClient {
 
 	Company getCompany(String id);
